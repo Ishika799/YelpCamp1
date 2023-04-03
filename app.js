878d46f298708c21ad011aa90a4bf726c43c9ef7
Ishika799 <87866744+Ishika799@users.noreply.github.com>
2023-04-03 16:41:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -38,8 +38,8 @@ app.use(methodOverride('_method'));
 const userRoutes=require('./routes/users');
 const campgroundRoutes=require('./routes/campgrounds');
 const reviewRoutes=require('./routes/reviews');
-// const dbUrl=process.env.DB_URL; 
-const dbUrl='mongodb://localhost:27017/yelp-camp'  
+const dbUrl=process.env.DB_URL||'mongodb://localhost:27017/yelp-camp'; 
+// const dbUrl='mongodb://localhost:27017/yelp-camp'  
 mongoose.connect(dbUrl,{
     useNewURLParser: true,
     // useCreateIndex:true,
